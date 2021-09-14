852444611762f6d001d613890e6d556f11c9f27c
Christian Muehlhaeuser <muesli@gmail.com>
2021-09-14 19:32:45 +0200

    Add tests for URL parsing

---
+++ git_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestCleanupURL(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected string
+	}{
+		{"git@github.com:muesli/gitty.git", "https://github.com/muesli/gitty"},
+		{"git://github.com/muesli/gitty.git", "https://github.com/muesli/gitty"},
+		{"http://github.com/muesli/gitty.git", "https://github.com/muesli/gitty"},
+		{"https://github.com/muesli/gitty", "https://github.com/muesli/gitty"},
+	}
+
+	for _, test := range tests {
+		r, err := cleanupURL(test.input)
+		if err != nil {
+			t.Errorf("Error: %s", err)
+		}
+		if r != test.expected {
+			t.Errorf("CleanupURL(%s) %s != %s", test.input, r, test.expected)
+		}
+	}
+}
